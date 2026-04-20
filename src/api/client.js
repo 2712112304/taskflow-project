efb4bec450e5f66803f72a04c3da89c501508eab
Carlos Jacier Romero Lopez <carlos.romero@alu.ceacfp.es>
2026-04-20 13:30:51 +0200

    feat: conecta frontend con backend en vercel

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/v1/tasks';
+const API_URL = 'https://taskflow-project-ufrm.vercel.app/api/v1/tasks';
 
 export async function getTasks() {
   const response = await fetch(API_URL);
